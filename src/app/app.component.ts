94cca69afa49a23093ac52f200d3a26f440e6850
sibongiseni <sibongiseni.ndebele@idolconsulting.co.za>
2021-03-11 15:20:11 +0200

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Employee} from './employee';
+import {EmployeeService} from './employee.service';
+import {HttpErrorResponse} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'employeemanagerapp';
+  public employees: Employee[];
+
+  constructor(private employeeService: EmployeeService) {}
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  public getEmployees(): void{
+    this.employeeService.getEmployees().subscribe(
+      (response: Employee[]) => {
+        this.employees = response;
+      },
+      // tslint:disable-next-line:no-shadowed-variable
+    (error: HttpErrorResponse) => {
+      alert(error.message);
+      }
+    );
+  }
+
+
 }
